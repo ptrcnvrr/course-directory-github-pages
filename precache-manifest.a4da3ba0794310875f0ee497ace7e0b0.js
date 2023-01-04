aadc5f9fc9b808139e1750db176ba94bdb8d1b5a
Patricia Navarro <patricia.navarro@codeandtheory.com>
2023-01-04 23:12:06 +0800

    Updates

---
+++ precache-manifest.a4da3ba0794310875f0ee497ace7e0b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f352cc0ffd5fe32860fa",
+    "url": "/course-directory-github-pages/static/css/main.0d044246.chunk.css"
+  },
+  {
+    "revision": "f352cc0ffd5fe32860fa",
+    "url": "/course-directory-github-pages/static/js/main.f352cc0f.chunk.js"
+  },
+  {
+    "revision": "25a97381f2fd20d18f35",
+    "url": "/course-directory-github-pages/static/js/1.25a97381.chunk.js"
+  },
+  {
+    "revision": "5d675c2afb2462cdba1e",
+    "url": "/course-directory-github-pages/static/js/runtime~main.5d675c2a.js"
+  },
+  {
+    "revision": "f54839165a8a24ebaa127b89a8269c30",
+    "url": "/course-directory-github-pages/index.html"
+  }
+];
